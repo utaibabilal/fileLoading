b46f67a557f2e6e002b6b1576a1da28ab6e32e4e
utaibabilal <mbk.ciit.bilalkhan@gmail.com>
2021-03-02 11:57:33 +0500

    final

---
+++ FileLoading/FileLoadingDlg.cpp
@@ -0,0 +1,249 @@
+
+// FileLoadingDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "FileLoading.h"
+#include "FileLoadingDlg.h"
+#include "afxdialogex.h"
+#include <string.h>
+
+
+
+
+
+
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CFileLoadingDlg dialog
+
+
+
+
+CFileLoadingDlg::CFileLoadingDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CFileLoadingDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+
+	  height = 0;
+	  width  = 0;
+}
+
+void CFileLoadingDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CFileLoadingDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTONLOAD, &CFileLoadingDlg::OnBnClickedButtonload)
+END_MESSAGE_MAP()
+
+
+// CFileLoadingDlg message handlers
+
+BOOL CFileLoadingDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+	
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CFileLoadingDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CFileLoadingDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CFileLoadingDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CFileLoadingDlg::OnBnClickedButtonload()
+{ 
+
+	 
+	  GetScreenHeightAndWidth();
+
+	     CString strIp = "";
+	     CString strPort = "";
+	     CString strPosX = "";
+		 CString strPosY = "";
+
+	  strIp  = GetValueByName("IP");
+	 strPort = GetValueByName("PORT"); 
+     strPosX = GetValueByName("POSX");
+	 strPosY = GetValueByName("POSY");
+
+	  
+	
+}
+
+
+CString CFileLoadingDlg::GetValueByName(CString strAttribute)
+{
+	CString filePath = "D:\\Amazonserver.xml";
+	xml.Load( filePath );
+	xml.ResetMainPos();
+
+	bool data = false;
+	CString strNameAttribute = "";
+	CString strAttributeValue = "";
+
+   while ( xml.FindElem("office") )
+   {
+			xml.IntoElem();		
+			while(xml.FindElem())
+			{
+				xml.IntoElem();	
+			 
+					while(xml.FindElem())
+					{
+						strNameAttribute =xml.GetAttrib("name");
+
+						if(strNameAttribute.CompareNoCase(strAttribute) == 0)
+						{
+						    strAttributeValue =  xml.GetAttrib("value"); 
+							break;
+						}
+					}	
+                 
+					xml.OutOfElem();
+					
+			}		
+   }	
+
+
+   return strAttributeValue;
+
+	
+   
+}  // end of function
+
+
+void CFileLoadingDlg:: GetScreenHeightAndWidth()
+{      
+	   int curPos = 0;
+	   CString strValue = "";
+	   strValue = GetValueByName("Resolution");
+	   
+	   CString resToken = (strValue.Tokenize(("X"), curPos));
+
+       height = atoi(resToken);
+
+	   while(!resToken.IsEmpty() && (curPos != resToken.GetLength()) )
+		{
+		   resToken = strValue.Tokenize(("X"), curPos);
+		   width = atoi(resToken);
+		   break;
+		   
+		}
+
+}
---
+++ FileLoading/FileLoadingDlg.h
@@ -0,0 +1,50 @@
+
+// FileLoadingDlg.h : header file
+#include "string.h"
+#include <vector>
+#include <cstdlib>
+#include "Markup.h"
+using namespace std;
+
+//
+
+#pragma once
+
+
+// CFileLoadingDlg dialog
+class CFileLoadingDlg : public CDialogEx
+{
+// Construction
+public:
+	CFileLoadingDlg(CWnd* pParent = NULL);	// standard constructor
+
+	CMarkup xml;
+
+// Dialog Data
+	enum { IDD = IDD_FILELOADING_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+
+	///===================variables=================
+	    int height;
+		int width;
+	//==============================================
+	afx_msg void OnBnClickedButtonload();
+	CString GetValueByName(CString strAttribute);
+	void GetScreenHeightAndWidth();
+	
+};
